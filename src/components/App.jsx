aa83fc7e4bb4e6edbdfb42d61a28bbacaefef4c4
Vallyblink <alabama.toj@gmail.com>
2023-06-06 04:46:06 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,7 @@ import {H1, H2} from './Hstyles'
 
 export class App extends Component{
   state = {
-    contacts: [ 
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: ''
   }
 
@@ -45,7 +41,7 @@ export class App extends Component{
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
     }
   }
-  
+
   render(){
       const {contacts, filter} = this.state;
       const filteredContacts = contacts.filter((contact) =>
